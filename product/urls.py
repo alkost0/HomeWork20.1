28a6917c93deeec3c72c8bfe321ce65001fa0a31
AlKost <134562029+alkost0@users.noreply.github.com>
2023-09-12 00:34:11 +0400

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -1,25 +1,13 @@
-"""
-URL configuration for config project.
+from django.urls import path
+from product.apps import CatalogConfig
+from product.views import index, contacts, category, category_products, product
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('category/', category, name='category'),
+    path('<int:pk>/category_products', category_products, name='category_products'),
+    path('<int:pk>/product/', product, name='product')
+]
