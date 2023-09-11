e8af9e571d163b0411819501f4d55d2ceeaa62d6
Alexey <alexey.kostenyu@gmail.com>
2023-09-11 23:25:52 +0400

    03 - fixed bugs for 20.2

---
+++ product/templatetags/product/my_tags.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def mymedia(val):
+    if val:
+        return f'/media/{val}'
+    return '#'
+
+@register.simple_tag
+def mymedia(val):
+    return f'/media/{val}'
